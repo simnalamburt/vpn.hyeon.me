d2b84a8601c1f100c900d9b8a6d4f45360672de0
Hyeon Kim <simnalamburt@gmail.com>
2019-04-14 15:45:02 +0900

    GCP에 VM 하나 띄움

--- main.tf
+++ main.tf
@@ -3,3 +3,21 @@ provider "google" {
   region  = "asia-northeast1"
   zone    = "asia-northeast1-c"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "personal-vpn"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config = {
+    }
+  }
+}
