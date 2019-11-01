8b674f722a57f050994c672d5e284afe02355fac
Hyeon Kim <simnalamburt@gmail.com>
2019-11-01 19:00:01 +0900

    gcp.tf: Turn off the VM

--- gcp.tf
+++ gcp.tf
@@ -1,15 +1,16 @@
-resource "google_compute_instance" "vm_instance" {
-  name         = "personal-vpn"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    access_config {
-    }
-  }
-}
+## TODO: 지금 안써서 잠깐 끔
+# resource "google_compute_instance" "vm_instance" {
+#   name         = "personal-vpn"
+#   machine_type = "f1-micro"
+#
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-9"
+#     }
+#   }
+#
+#   network_interface {
+#     access_config {
+#     }
+#   }
+# }
